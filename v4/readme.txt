08083e06cc05118ca626495a4d6602617e428e53
Nazareth-Aguilera <NazWebDev@gmail.com>
2023-09-20 18:30:21 -0700

    git init commit

---
+++ v4/readme.txt
@@ -0,0 +1,1 @@
+# using Version 4
---
+++ v4/scripts/home.js
@@ -0,0 +1,28 @@
+// this must be the last script in index.html
+
+(function() {
+
+    // root element
+    const root = document.getElementById("root");
+
+
+
+    // initialize components
+    const mobileNavbar = new MobileNavbar("../resources/skeletor.svg", ["Home", "About Me"]);
+
+    // add components you want to render to components array
+    const components = [
+        mobileNavbar
+    ];
+
+
+    // this function renders components into the root element
+    render(root, components);
+
+})();
+
+function render(root, components) {
+    for (let component of components) {
+        root.appendChild(component.node);
+    }
+}
